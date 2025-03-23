ceb7e57925c8a2d0e2533e6abbcfd94fca0b1367
Redboyrave <gabrielgreis94@hotmail.com>
2025-03-23 17:54:38 -0300

    Dice are now instaced dinamically. Added editor controls for ammount of initial dice and (temporarily) display area.

--- liars-dice/tray.gd
+++ liars-dice/tray.gd
@@ -1,15 +1,37 @@
 extends Node2D
 
-@onready var dice1 = %Dice1
-@onready var dice2 = %Dice2
-@onready var dice3 = %Dice3
-@onready var dice4 = %Dice4
-@onready var dice5 = %Dice5
-@onready var dice6 = %Dice6
+const DICE_SCENE:PackedScene = preload("res://dice.tscn")
 
-@onready var tray
+@export var dice_ammount:int = 6;
+@export var tray_size:Vector2 = Vector2(256,512); ## TEMPORARY, MAYBE MAKE A RETANGLE IN EDITOR TO DEFINE THIS
+
+var tray:Array[Dice] = []
+var results:Array[int] = []
+
+func _ready()->void:
+	for i in range(dice_ammount):
+		var die = DICE_SCENE.instantiate();
+		tray.append( die );
+		self.add_child( die );
+	position_dice()
 
 func _input(event):
+	## Só comentar aqui que, idealmente, a gente deveria unificar o input da bandeja e do dado,
+	## para ser uma coisa só, fica mais facil de fazer a manutenção eu acho.
 	if event.is_action_pressed("throw"):
-		tray = [dice1.face, dice2.face, dice3.face, dice4.face, dice5.face, dice6.face]
-		print(tray)
+		results.clear() ##Otherwise it keeps acumullating;
+		for die in tray:
+			results.append(die.face);
+		##Old implementation, for reference;
+		#tray = [dice1.face, dice2.face, dice3.face, dice4.face, dice5.face, dice6.face]
+		print(results)
+
+func position_dice():
+	var space
+	## For now two dice per row;
+	for i in range(tray.size()):
+		## (numero do dado % numero de colunas) * (lagura da bandeja / (numero de dados/ ???))
+		tray[i].position.x = (i%2) * tray_size.x; #Nesse caso, como o resultado só pode ser 0 e 1, não tem problema ser só "(* tray_size.x)"
+		## (numero do dado/numero de colunas) * (altura da bandeja / (numero de dados/numero de colunas))
+		tray[i].position.y = (i/2) * (tray_size.y/(dice_ammount/2));
+	pass
